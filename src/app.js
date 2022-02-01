23c267e9d863c7b5c1a57bcabb92df3b36345e41
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-01 17:59:53 +0100

    created an express server get request to read api audio sanitizing inputs by destructuring voice and scriptText

---
+++ src/app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const apiaudio = require("apiaudio").default;
+
+apiaudio.configure({ apiKey: process.env.APIKEY, debug: false });
+
+const app = express();
+
+//middleware
+app.use(express.json());
+
+//Get the audio from the api
+app.get("/api/audio", async (req, res) => {
+  // sanitizing the inputs
+  const { scriptText, voice } = req.body;
+
+  try {
+    const script = await apiaudio.Script.create({
+      scriptText: scriptText,
+      scriptName: "hello",
+      projectName: "hello",
+      moduleName: "hello",
+    });
+    console.log(script);
+
+    const speech = await apiaudio.Speech.create({
+      scriptId: script["scriptId"],
+      voice: voice,
+    });
+    console.log(speech);
+
+    res.status(200).send(speech);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error happened!");
+  }
+});
+
+module.exports = app;
