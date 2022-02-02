cabef6201e0e2a28a0aba9987c448f6e4fccc659
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-02 16:21:00 +0100

    Added the audio element with audio tag to play on the FE

---
+++ client/src/components/molecules/AudioElement.js
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import styled from "styled-components";
+
+const StyledMain = styled.div`
+  border: 2px solid rgb(7, 170, 7);
+  border-radius: 5px;
+  padding: 10px;
+`;
+
+const AudioElement = () => {
+  const inputEl = useRef(null);
+  // const onButtonClick = () => {
+  //   inputEl.current.focus();
+  // };
+  return (
+    <StyledMain>
+      <h1>🥳 Success - Your Ad is ready</h1>
+      <p>Beautiful, you are now ready for prime time!</p>
+      <div>
+        <audio
+          input={inputEl.toString()}
+          ref={inputEl}
+          type="text"
+          src="https://v1.api.audio/url/4c6a7/hello.mp3"
+          controls
+          autoPlay
+        />
+        <button>Download / Copy URL</button>
+      </div>
+    </StyledMain>
+  );
+};
+
+export default AudioElement;
