6b522e5dcf90bdfc6cba6ec60daf47d5fa6904f0
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-02 18:37:25 +0100

    imported apiprovider, queryClient and provider wrapped the App with the providers

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,20 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { LayoutProvider } from "./context/LayoutProvider";
+import APIProvider from "./context/APIProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <LayoutProvider>
-      <App />
-    </LayoutProvider>
+    <QueryClientProvider client={queryClient}>
+      <APIProvider>
+        <LayoutProvider>
+          <App />
+        </LayoutProvider>
+      </APIProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
