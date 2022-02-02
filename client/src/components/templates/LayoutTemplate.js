96282cc5b36fd04cf73222d3222dc6ac359d2ba8
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-02 11:02:49 +0100

    Added LayoutTemplate from AntD

---
+++ client/src/components/templates/LayoutTemplate.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Layout, Menu } from "antd";
+import FormTemplate from "./FormTemplate";
+import { useContext } from "react";
+import { LayoutContext } from "../../context/LayoutProvider";
+// import { useHistory } from "react";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from "@ant-design/icons";
+
+const { Header, Sider, Content } = Layout;
+
+const LayoutTemplate = () => {
+  // let history = useHistory();
+  const { collapsed, setCollapsed } = useContext(LayoutContext);
+
+  const toggleSidebar = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+          <Menu.Item key="1" icon={<UserOutlined />}>
+            nav 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+            nav 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UploadOutlined />}>
+            nav 3
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              onClick: toggleSidebar,
+            }
+          )}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          <FormTemplate />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default LayoutTemplate;
