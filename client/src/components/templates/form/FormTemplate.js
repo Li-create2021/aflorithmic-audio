11dcadfcc0ce2db2a6dffcaa828c5c8c627344ae
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-03 15:53:03 +0100

    deleted unused imports passed context added event handlers for voice, sound and form submit added function for getting dynamic data from api inserted onFinish into from changed all 'names' in form to appropriate ones deleted the button I didn't need

--- client/src/components/templates/form/FormTemplate.js
+++ client/src/components/templates/form/FormTemplate.js
@@ -1,31 +1,50 @@
 import React, { useContext } from "react";
 import { Form, Input, Button, Select } from "antd";
 import AudioElement from "../../molecules/AudioElement";
-// import APIProvider from "../../../context/APIProvider";
 import { ApiContext } from "../../../context/APIProvider";
 
 const { TextArea } = Input;
 
 const FormTemplate = () => {
-  //pass usestate props from getUrl
-  // const GetUrl = ({ getUrl, setGetUrl }) => {};
+  //All data is stored in a context to be accessable globally
+  const apiContext = useContext(ApiContext);
 
-  const { createNewAudio, getUrl, setGetUrl } = useContext(ApiContext);
-  console.log(ApiContext.createNewAudio);
+  //Event handler for selecting the speaker/voice
+  function voiceHandleChange(value) {
+    console.log(`selected ${value}`);
+  }
 
-  // on form submit, execute this
-  const onFormSubmit = async (e) => {
-    e.preventDefault();
-    setGetUrl(getUrl);
+  //Event handler for selecting sound template
+  function SoundDesignHandleChange(value) {
+    console.log(`selected ${value}`);
+  }
 
-    const audio = ApiContext.createNewAudio.mutateAsync({
-      voice: "Joanna", // can come from a state or from the submit event
-      speech: "hey",
-    });
+  // on form submit, execute this handler to get the audio
+  const onSubmitHandler = async (values) => {
+    // e.preventDefault();
 
-    console.log(audio);
+    const scriptText = (
+      values.introduction +
+      values.mainCopy +
+      values.cta
+    ).toUpperCase();
 
-    // setTaco(audio.url) state the audio url
+    // let script = await apiaudio.Script.create({ scriptText: scriptText });
+    // console.log(script);
+
+    const audioData = {
+      scriptText: scriptText,
+      voice: values.speaker,
+      soundDesign: values.soundTemplate,
+    };
+    console.log(audioData);
+
+    //execute post request
+    const audio = await apiContext.createNewAudio.mutateAsync(audioData);
+
+    console.log(audio.data.default.url);
+
+    apiContext.setGetUrl(audio.data.default.url);
   };
 
   return (
@@ -37,52 +56,50 @@ const FormTemplate = () => {
         span: 25,
       }}
       layout="horizontal"
-      // initialValues={{
-      //   size: componentSize,
-      // }}
-      // onValuesChange={onFormLayoutChange}
-      // size={componentSize}
+      onFinish={onSubmitHandler}
     >
-      <Form.Item label="Introduction">
+      <Form.Item label="Introduction" name="introduction">
         <TextArea
           rows={2}
           placeholder="Write a sweet but short intro for your audio"
         />
       </Form.Item>
-      <Form.Item label="Main Copy">
+      <Form.Item label="Main Copy" name="mainCopy">
         <TextArea
           rows={4}
           placeholder="Explain your product in less than 50 words"
         />
       </Form.Item>
-      <Form.Item label="Call to action">
+      <Form.Item label="Call to action" name="cta">
         <TextArea rows={2} placeholder="Say goodbye with a CTA" />
       </Form.Item>
-      <Form.Item label="Speaker">
-        <Select>
-          <Select.Option value="demo">Mia</Select.Option>
-          <Select.Option value="demo">Libby</Select.Option>
-          <Select.Option value="demo">Guy</Select.Option>
+      <Form.Item label="Speaker" name="speaker">
+        <Select onChange={voiceHandleChange}>
+          <Select.Option value="Mia">Mia</Select.Option>
+          <Select.Option value="Libby">Libby</Select.Option>
+          <Select.Option value="Guy">Guy</Select.Option>
         </Select>
       </Form.Item>
-      <Form.Item label="Sound Design">
-        <Select>
-          <Select.Option value="demo">
+      <Form.Item label="Sound Design" name="soundTemplate">
+        <Select onChange={SoundDesignHandleChange}>
+          <Select.Option value="Newhorizons">
             New Horizons(uptempo electronic tunes)
           </Select.Option>
-          <Select.Option value="demo">
+          <Select.Option value="Openup">
             Open Up (emotional acoustic piano){" "}
           </Select.Option>
-          <Select.Option value="demo">
+          <Select.Option value="Fairytale">
             Fairy Tale (acoustic music for kid's stories)
           </Select.Option>
         </Select>
       </Form.Item>
-      <Form.Item label="">
+      <Form.Item>
         <AudioElement />
       </Form.Item>
-      <Form.Item label="">
-        <Button>PRODUCE & PREVIEW AUDIO</Button>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          PRODUCE & PREVIEW AUDIO
+        </Button>
       </Form.Item>
     </Form>
   );
