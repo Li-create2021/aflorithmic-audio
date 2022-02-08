f64cc71d12364ef3122e852ce917363281ea112b
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-08 08:24:03 +0100

    rendering layouttemplate on home page

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import LayoutTemplate from "../templates/layout/LayoutTemplate";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <h2>Home</h2> */}
+      <LayoutTemplate />
+    </div>
+  );
+};
+
+export default Home;
