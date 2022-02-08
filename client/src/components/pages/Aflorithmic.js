18886a6b71af65c178f44d640ef01407df5d8cc7
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-08 23:43:57 +0100

    rendering layouttemplate component

--- client/src/components/pages/Aflorithmic.js
+++ client/src/components/pages/Aflorithmic.js
@@ -1,13 +1,14 @@
 import React from "react";
-import Title from "antd/lib/typography/Title";
+// import Title from "antd/lib/typography/Title";
+import LayoutTemplate from "../templates/layout/LayoutTemplate";
 
 // import LayoutTemplate from "../templates/layout/LayoutTemplate";
 
 const Aflorithmic = () => {
   return (
     <div>
-      <Title level={4}>Aflorithmic Audio As a Service</Title>
-      {/* <LayoutTemplate /> */}
+      {/* <Title level={4}>Aflorithmic Audio As a Service</Title> */}
+      <LayoutTemplate />
     </div>
   );
 };
