d8dc9cb71f2f242f06bc28c08d7110efe0951e2d
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-08 22:11:54 +0100

    added a function to edit later

---
+++ client/src/components/pages/Aflorithmic.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Title from "antd/lib/typography/Title";
+
+// import LayoutTemplate from "../templates/layout/LayoutTemplate";
+
+const Aflorithmic = () => {
+  return (
+    <div>
+      <Title level={4}>Aflorithmic Audio As a Service</Title>
+      {/* <LayoutTemplate /> */}
+    </div>
+  );
+};
+
+export default Aflorithmic;
