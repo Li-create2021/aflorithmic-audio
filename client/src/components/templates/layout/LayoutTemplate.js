a184c1b85f811e20707e97e5e4a3b89a4f6d0d0b
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-08 17:05:51 +0100

    useLocation for sidebar menu items, added an avatar in header added submenu items for projects

--- client/src/components/templates/layout/LayoutTemplate.js
+++ client/src/components/templates/layout/LayoutTemplate.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { Layout, Menu } from "antd";
-import FormTemplate from "../form/FormTemplate";
 import { useContext } from "react";
+import { useLocation, Link } from "react-router-dom";
+import { Layout, Menu, Avatar } from "antd";
+import FormTemplate from "../form/FormTemplate";
+import Title from "antd/lib/typography/Title";
 import { LayoutContext } from "../../../context/LayoutProvider";
 import "./LayoutStyles.css";
-// import { useHistory } from "react";
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
+  MailOutlined,
+  PlayCircleOutlined,
 } from "@ant-design/icons";
 
 const { Header, Sider, Content } = Layout;
+const { SubMenu } = Menu;
 
 const LayoutTemplate = () => {
-  // let history = useHistory();
   const { collapsed, setCollapsed } = useContext(LayoutContext);
+  const location = useLocation();
 
   const toggleSidebar = () => {
     setCollapsed(!collapsed);
@@ -26,21 +28,49 @@ const LayoutTemplate = () => {
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
+        <Title style={{ color: "white" }} level={5}>
+          Liana van Zyl
+        </Title>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={[location.pathname]}
+        >
+          <Menu.Item key="/" icon={<UserOutlined />}>
+            <span>About Liana</span>
+            <Link to="/"></Link>
           </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
+          <SubMenu
+            key="/projects"
+            icon={<PlayCircleOutlined />}
+            title="Liana's Projects"
+          >
+            <Menu.Item key="/projects/aflorithmic">
+              <span>Aflorithmic</span>
+              <Link to="/projects/aflorithmic"></Link>
+            </Menu.Item>
+            {/* <Menu.Item key="6">Option 6</Menu.Item>
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item> */}
+          </SubMenu>
+          <Menu.Item key="/contact" icon={<MailOutlined />}></Menu.Item>
         </Menu>
       </Sider>
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }}>
+          <Avatar
+            size={{
+              xs: 24,
+              sm: 32,
+              md: 40,
+              lg: 64,
+              xl: 80,
+              xxl: 100,
+            }}
+            style={{ float: "right", margin: 15 }}
+            src="/././user.png"
+          />
+
           {React.createElement(
             collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
             {
