fea48d822bc79619f53ed5ec26dbf421b2ff9005
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-09 01:44:19 +0100

    broke the component up into smaller more scalable and maintainable code blocks

--- client/src/components/templates/layout/LayoutTemplate.js
+++ client/src/components/templates/layout/LayoutTemplate.js
@@ -1,96 +1,9 @@
 import React from "react";
-import { useContext } from "react";
-import { useLocation, Link } from "react-router-dom";
-import { Layout, Menu, Avatar } from "antd";
-import FormTemplate from "../form/FormTemplate";
-import { LayoutContext } from "../../../context/LayoutProvider";
+import { Layout } from "antd";
 import "./LayoutStyles.css";
-import {
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
-  UserOutlined,
-  MailOutlined,
-  PlayCircleOutlined,
-} from "@ant-design/icons";
-
-const { Title } = Typography;
-const { Header, Sider, Content } = Layout;
-const { SubMenu } = Menu;
 
 const LayoutTemplate = () => {
-  const { collapsed, setCollapsed } = useContext(LayoutContext);
-  const location = useLocation();
-
-  const toggleSidebar = () => {
-    setCollapsed(!collapsed);
-  };
-
-  return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <Avatar
-          size={40}
-          style={{ float: "right", margin: 15 }}
-          src="/././user.png"
-        />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={[location.pathname]}
-        >
-          <Menu.Item key="/" icon={<UserOutlined />}>
-            <span>About Liana</span>
-            <Link to="/"></Link>
-          </Menu.Item>
-          <SubMenu
-            key="/projects"
-            icon={<PlayCircleOutlined />}
-            title="Liana's Projects"
-          >
-            <Menu.Item key="/projects/aflorithmic">
-              <span>Aflorithmic</span>
-              <Link to="/projects/aflorithmic"></Link>
-            </Menu.Item>
-            {/* <Menu.Item key="6">Option 6</Menu.Item>
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item> */}
-          </SubMenu>
-          <Menu.Item key="/contact" icon={<MailOutlined />}>
-            <span>Contact</span>
-            <Link to="/contact"></Link>
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}></Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}></Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }}>
-          {React.createElement(
-            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-            {
-              className: "trigger",
-              onClick: toggleSidebar,
-            }
-          )}
-        </Header>
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          <Title level={3}>
-            Self-Service Audio Ad Builder from Aflorithmic
-          </Title>
-          <Title level={5}>1. Write Your Ad</Title>
-          <Title level={5}>2. Select Your Speaker & Add Music</Title>
-          <FormTemplate />
-        </Content>
-      </Layout>
-    </Layout>
-  );
+  return <Layout style={{ background: "#fff" }}></Layout>;
 };
 
 export default LayoutTemplate;
