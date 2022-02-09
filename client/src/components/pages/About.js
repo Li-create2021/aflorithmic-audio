faecd2b82861e55f22c9cc8dfdd650aa14352ca0
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-09 01:46:25 +0100

    added layout

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,8 +1,21 @@
 import React from "react";
-import ResumeDrawer from "../organisms/drawer/ResumeDrawer";
+import { Layout } from "antd";
+import { AppFooter } from "../molecules/AppFooter";
+import SideBarMenu from "../organisms/SideBarMenu";
+import AppHeader from "../molecules/AppHeader";
+import AboutPageContents from "../molecules/AboutPageContents";
 
 const About = () => {
-  return <ResumeDrawer />;
+  return (
+    <Layout style={{ background: "#fff" }}>
+      <SideBarMenu />
+      <Layout>
+        <AppHeader />
+        <AboutPageContents />
+        <AppFooter />
+      </Layout>
+    </Layout>
+  );
 };
 
 export default About;
