7a7a9806c2b357651537c51734a9ef6c234be04d
Liana <82238565+Li-create2021@users.noreply.github.com>
2022-02-09 01:48:03 +0100

    added app footer

---
+++ client/src/components/molecules/AppFooter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Layout } from "antd";
+
+const { Footer } = Layout;
+
+export const AppFooter = () => {
+  return (
+    <Footer style={{ textAlign: "center" }}>
+      {" "}
+      ©2022 Created by Liana van Zyl
+    </Footer>
+  );
+};
